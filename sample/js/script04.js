24c90a08388cbe461b3150f1898d29b07bb6a408
YUYONGMIN <y-yu2278@st.kyoto-art.ac.jp>
2022-11-11 15:55:29 +0900

    anime test

--- sample/js/script04.js
+++ sample/js/script04.js
@@ -5,3 +5,14 @@ $(function(){
     columnWidth: 200
   });
 })
+
+//スクロールしたりウィンドウサイズを変えた時に実行
+$(window).on("scroll resize load",function(){
+  windowH = $(this).height()//ウィンドウの高さ
+  documentH = $(document).height()//内容の高さ
+  documentW = $(document).width()//内容の幅
+  scrollTop = $(this).scrollTop()//何ピクセルスクロールしたか
+  scrollRatio = scrollTop/(documentH-windowH)//どれくらいスクロールされたか0から1
+  
+  separate = documentH / 3;//内容の高さを3分割した数値
+})
